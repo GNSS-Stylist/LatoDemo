f0401e431cd05605cede09a8c55db829ae6910a6
GNSS-Stylist <gnssstylist@sci.fi>
2023-04-06 00:13:08 +0300

    Add wrecking and breaking and strip some strips

--- Data/Satellite/Wreck/SatelliteWreck.gd
+++ Data/Satellite/Wreck/SatelliteWreck.gd
@@ -3,6 +3,12 @@ extends Node3D
 
 @export var scopeLightEnergy:float = 1
 @export var scopeLightColor:Color = Color(0, 1, 0)
+@export var randomDebrisMaxLinearVelocity:Vector3 = Vector3(0.03, 0.03, 0.03)
+@export var randomDebrisMaxAngularVelocity:Vector3 = Vector3(0.1, 0.1, 0.1)
+@export var randomDebrisMaxPos:Vector3 = Vector3(3, 3, 3)
+
+var RigidBodySolarCell = preload("res://Data/Satellite/Wreck/RigidBodySolarCell.tscn")
+#var DebrisField = preload("res://Data/Satellite/Wreck/DebrisField.tscn")
 
 var mainWreckRotationMutex:Mutex = Mutex.new()
 @export var mainWreckRotation:float = 0:
@@ -14,6 +20,49 @@ var mainWreckRotationMutex:Mutex = Mutex.new()
 	get:
 		return mainWreckRotation
 
+@export var randomDebrisSeed:int = 0
+
+@export var regenDebrisIfNeeded:bool = false:
+	set(trig):
+		print("Triggered")
+		if (physicsProcessCalledAfterRegeneratingDebris && trig):
+			# Only regenerate debris if physics process was run after
+			# last generation. This is to prevent regenerating them unnecessarily on the
+			# fly, causing a stutter (they are generated on originally on _ready)
+			# This was added to be able to rewind
+			regeneratePredefinedDebris()
+			regenerateRandomDebris()
+			physicsProcessCalledAfterRegeneratingDebris = false
+			regenDebrisIfNeeded = trig
+			print("Wreck: (Re)generating debris")
+	get:
+		return regenDebrisIfNeeded
+
+@export var dbg_RegenDebris_Force:bool = false:
+	set(trig):
+		if (trig):
+			regeneratePredefinedDebris()
+			regenerateRandomDebris()
+			physicsProcessCalledAfterRegeneratingDebris = false
+	get:
+		return 0
+
+var physicsProcessCalledAfterRegeneratingDebris:bool = false
+
+func _ready():
+	regeneratePredefinedDebris()
+	regenerateRandomDebris()
+	physicsProcessCalledAfterRegeneratingDebris = false
+	
+#	myRandInit(2314234)
+	
+#	for i in range(150):
+#		var newSolarCell = RigidBodySolarCell.instantiate()
+#		newSolarCell.transform = Transform3D(Basis(Vector3(myRandf(), myRandf(), myRandf()), Vector3(myRandf(), myRandf(), myRandf()), Vector3(myRandf(), myRandf(), myRandf())).orthonormalized(), Vector3(myRandf_range(-3, 3), myRandf_range(-1, 1), myRandf_range(-3, 3)))
+#		newSolarCell.linear_velocity = Vector3(myRandf_range(-0.1, 0.1), myRandf_range(-0.1, 0.1), myRandf_range(-0.1, 0.1))
+#		newSolarCell.angular_velocity = Vector3(myRandf_range(-0.1, 0.1), myRandf_range(-0.1, 0.1), myRandf_range(-0.1, 0.1))
+#		$Debris.add_child(newSolarCell)
+
 func _process(delta):
 	if ((!Global) ||(Engine.is_editor_hint() && Global.cleanTempToolData)):
 		return
@@ -21,6 +70,7 @@ func _process(delta):
 	$MainWreck/DishAntenna/ScopeLight.visible = true
 	$MainWreck/DishAntenna/ScopeLight.light_energy = Global.lowPassFilteredSoundAmplitudeData[Global.masterReplayTime * 8000] * scopeLightEnergy
 	$MainWreck/DishAntenna/ScopeLight.light_color = scopeLightColor
+	
 
 var oldRotation:float = 0
 func _physics_process(delta):
@@ -29,5 +79,70 @@ func _physics_process(delta):
 	mainWreckRotationMutex.unlock()
 
 	if (newRotation != oldRotation):
-		$MainWreck.rotation_degrees = Vector3(0, newRotation, 0)
+		$MainWreck.rotation_degrees = Vector3(0, newRotation, -45)
 		oldRotation = newRotation
+	
+	if (!Engine.is_editor_hint()):
+#		print("Wreck physics_process called, enabled")
+		physicsProcessCalledAfterRegeneratingDebris = true
+	
+
+func regeneratePredefinedDebris():
+	var predefinedDebris:Node3D = get_node_or_null("PredefinedDebris")
+	
+	if (predefinedDebris):
+		self.remove_child(predefinedDebris)
+		predefinedDebris.queue_free()
+	
+#	predefinedDebris = DebrisField.instantiate()
+	var debrisScene = load("res://Data/Satellite/Wreck/DebrisField.tscn")
+	predefinedDebris = debrisScene.instantiate()
+	
+	predefinedDebris.name = "PredefinedDebris"
+#	predefinedDebris.transform.basis = Basis.IDENTITY.rotated(Vector3(1,0,0), deg_to_rad(45))
+	
+	self.add_child(predefinedDebris)
+
+func regenerateRandomDebris():
+	myRandInit(randomDebrisSeed)
+
+	var randomDebris = get_node_or_null("RandomDebris")
+
+	if (randomDebris):
+		self.remove_child(randomDebris)
+		randomDebris.queue_free()
+
+	randomDebris = Node3D.new()
+	randomDebris.name = "RandomDebris"
+
+	for i in range(150):
+		var newSolarCell = RigidBodySolarCell.instantiate()
+		newSolarCell.transform = Transform3D(Basis(Vector3(myRandf(), myRandf(), myRandf()), Vector3(myRandf(), myRandf(), myRandf()), Vector3(myRandf(), myRandf(), myRandf())).orthonormalized(), Vector3(myRandf_range(-randomDebrisMaxPos.x, randomDebrisMaxPos.x), myRandf_range(-randomDebrisMaxPos.y, randomDebrisMaxPos.y), myRandf_range(-randomDebrisMaxPos.z, randomDebrisMaxPos.z)))
+		newSolarCell.linear_velocity = Vector3(myRandf_range(-randomDebrisMaxLinearVelocity.x, randomDebrisMaxLinearVelocity.x), myRandf_range(-randomDebrisMaxLinearVelocity.y, randomDebrisMaxLinearVelocity.y), myRandf_range(-randomDebrisMaxLinearVelocity.z, randomDebrisMaxLinearVelocity.z))
+		newSolarCell.angular_velocity = Vector3(myRandf_range(-randomDebrisMaxAngularVelocity.x, randomDebrisMaxAngularVelocity.x), myRandf_range(-randomDebrisMaxAngularVelocity.y, randomDebrisMaxAngularVelocity.y), myRandf_range(-randomDebrisMaxAngularVelocity.z, randomDebrisMaxAngularVelocity.z))
+		randomDebris.add_child(newSolarCell)
+
+	self.add_child(randomDebris)
+
+# As Godot doesn't provide reproducible random numbers (across versions),
+# let's introduce our own. No need for high quality here.
+# Source: https://en.wikipedia.org/wiki/Linear_congruential_generator
+# or: Numerical Recipes from the "quick and dirty generators" list, 
+# Chapter 7.1, Eq. 7.1.6 parameters from Knuth and H. W. Lewis
+
+var randVal:int = 0
+
+func myRandInit(seed:int):
+	randVal = seed & 0xFFFFFFFF
+
+func myRandGetInt() -> int:
+	randVal = (randVal * 1664525 + 1013904223) & 0xFFFFFFFF;
+	return randVal
+
+func myRandf() -> float:
+	var u:int = myRandGetInt()
+	return u / float(4294967296)
+
+func myRandf_range(minVal:float, maxVal:float) -> float:
+	var rand01 = myRandf()
+	return minVal + rand01 * (maxVal - minVal)
