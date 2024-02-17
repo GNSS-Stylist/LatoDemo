1c80b33c3351b2e9e36f9c8ca88a119a586e7f16
GNSS-Stylist <gnssstylist@sci.fi>
2024-02-17 16:03:09 +0200

    Add panel to show/hide other panels (like help) and objects
    Once again I should have done this much earlier... Also added more ships to debug replayer and other minor fixes

---
+++ Data/Main/Panel_Help.gd
@@ -0,0 +1,69 @@
+extends Panel
+
+@onready var node_Keys:CanvasItem = get_node("../Panel_Keys")
+@onready var chkBox_ShowKeys:CheckBox = $CheckBox_ShowKeys
+
+@onready var node_CurrentCameraTransform:CanvasItem = get_node("../Panel_CurrentCameraTransform")
+@onready var chkBox_ShowCurrentCameraTransform:CheckBox = $CheckBox_ShowCurrentCameraTransform
+
+@onready var node_StatusBar:CanvasItem = get_node("../Panel_Bottom")
+@onready var chkBox_ShowStatusBar:CheckBox = $CheckBox_ShowStatusBar
+
+@onready var chkBox_ShowDebugShips:CheckBox = $CheckBox_ShowDebugShips
+var debugShipsWereVisible:bool = false
+
+@onready var OptionButton_TrackReplayerShip:OptionButton = $OptionButton_TrackReplayerShip
+var lastTrackReplayerShip = 0
+
+func _ready():
+	if (!Engine.is_editor_hint()):
+		# Clearing these when running the demo as these may be set
+		# on the editor for debugging purposes
+		setEliteDebugShipVisibility(false)
+		setEliteTrackReplayerShip(0)
+
+func _process(delta):
+	if (Input.is_action_just_pressed("show_help")):
+		self.visible = !self.visible
+	
+	node_Keys.visible = chkBox_ShowKeys.button_pressed
+	node_CurrentCameraTransform.visible = chkBox_ShowCurrentCameraTransform.button_pressed
+	node_StatusBar.visible = chkBox_ShowStatusBar.button_pressed
+	
+	if (chkBox_ShowDebugShips.button_pressed != debugShipsWereVisible):
+		debugShipsWereVisible = chkBox_ShowDebugShips.button_pressed
+		setEliteDebugShipVisibility(chkBox_ShowDebugShips.button_pressed)
+	
+	if (OptionButton_TrackReplayerShip.get_selected_id() != lastTrackReplayerShip):
+		lastTrackReplayerShip = OptionButton_TrackReplayerShip.get_selected_id()
+		setEliteTrackReplayerShip(OptionButton_TrackReplayerShip.get_selected_id())
+	
+func setEliteDebugShipVisibility(visible_p:bool):
+	get_node("../../Elite/ShipDrawMeshes/CobraMKIII/DebugCobra").visible = visible_p
+	get_node("../../Elite/ShipDrawMeshes/Python/DebugPython").visible = visible_p
+	get_node("../../Elite/ShipDrawMeshes/Thargoid/DebugThargoid").visible = visible_p
+	get_node("../../Elite/ShipDrawMeshes/Viper/DebugViper").visible = visible_p
+
+func setEliteTrackReplayerShip(ship:int):
+	get_node("../../Elite/DebugShipTrackReplayer/CobraMkIII").visible = false
+	get_node("../../Elite/DebugShipTrackReplayer/Python").visible = false
+	get_node("../../Elite/DebugShipTrackReplayer/Viper").visible = false
+	get_node("../../Elite/DebugShipTrackReplayer/Thargoid").visible = false
+	get_node("../../Elite/DebugShipTrackReplayer").visible = true
+
+	match (ship):
+		[0, _]:
+			get_node("../../Elite/DebugShipTrackReplayer").visible = false
+		1:
+			get_node("../../Elite/DebugShipTrackReplayer/CobraMkIII").visible = true
+		2:
+			get_node("../../Elite/DebugShipTrackReplayer/Python").visible = true
+		3:
+			get_node("../../Elite/DebugShipTrackReplayer/Viper").visible = true
+		4:
+			get_node("../../Elite/DebugShipTrackReplayer/Thargoid").visible = true
+
+			
+
+func _on_button_close_pressed():
+	visible = false
